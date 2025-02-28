0e916ea86d11e7cd0b16048b7dfd245eb066a621
Murat Erkılıç <43292855+muraterkilic@users.noreply.github.com>
2025-02-28 10:32:09 +0300

    Junit kullanımı BeforeEach ve AfterEach

--- src/test/java/com/muraterkilic/starter/ExceptionManagementApplicationTests.java
+++ src/test/java/com/muraterkilic/starter/ExceptionManagementApplicationTests.java
@@ -1,13 +1,35 @@
 package com.muraterkilic.starter;
 
+import com.muraterkilic.dto.EmployeeDto;
+import com.muraterkilic.service.IEmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ExceptionManagementStarter.class})
 class ExceptionManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private IEmployeeService employeeService;
+
+
+	@BeforeEach
+	public void beforeEach() {
+		System.out.println("before each çalıltı");
 	}
 
+	@Test
+	public void testFindEmployeeById() {
+		EmployeeDto employeeDto = employeeService.findEmployeeById(1L);
+		if (employeeDto != null){
+			System.out.println("isim : " + employeeDto.getName());
+		}
+	}
+
+	@AfterEach
+	public void afterEach() {
+		System.out.println("after each çalıltı");
+	}
 }
